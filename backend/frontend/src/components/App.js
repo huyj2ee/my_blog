8a1363330c833163ba24914731e6604295314006
huyj2ee <huyj2ee@outlook.com>
2023-04-24 15:55:37 +0700

    [frontend] refactor to move App component to seperate file App.js

---
+++ backend/frontend/src/components/App.js
@@ -0,0 +1,130 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { AiOutlineDown, AiOutlineSearch } from "react-icons/ai";
+import {
+  Route,
+  Routes,
+  Link,
+  useLocation
+} from 'react-router-dom';
+import { BLOG_ID, ROOT_ID, API_KEY } from '../utils/constant';
+import HomePage from './HomePage';
+import Contact from './Contact';
+import CV from './CV';
+import ProjectsList from './ProjectsList';
+import Project from './Project';
+import BlogsList from './BlogsList';
+import Blog from './Blog';
+
+const dispatchToPropsAppMap =
+  dispatch => {
+    return {
+      loadRoot: () => {
+        dispatch(
+          {
+            type: 'LOAD_ROOT',
+            payload: {
+              request: {
+                method: 'GET',
+                url: '/' + BLOG_ID + '/posts/' + ROOT_ID + '?key=' + API_KEY,
+                data: {
+                }
+              }
+            } 
+          }
+        )
+      },
+      clearProjectsList: () => {
+        dispatch(
+          {
+            type: 'CLEAR_PROJECTS_LIST'
+          }
+        )
+      },
+      clearBlogsList: () => {
+        dispatch(
+          {
+            type: 'CLEAR_BLOGS_LIST'
+          }
+        )
+      }
+    }
+  };
+
+const stateToPropsAppMap = (state) => {
+  return {
+      projectsList: state.projectsList,
+      blogsList: state.blogsList
+  };
+};
+
+const App = connect(stateToPropsAppMap, dispatchToPropsAppMap)(
+  function (props) {
+    const location = useLocation();
+
+    if(location.pathname !== '/projs' && location.pathname !== '/proj' && props.projectsList && typeof props.clearProjectsList === 'function')
+      props.clearProjectsList();
+
+    if(location.pathname !== '/blogs' && location.pathname !== '/blog' && props.blogsList && typeof props.clearBlogsList === 'function')
+      props.clearBlogsList();
+
+    useEffect(()=>{
+      if (typeof props.loadRoot === 'function')
+        props.loadRoot();
+    },[props]);
+
+    return (
+      <div id="main">
+        <div id="header">
+          {/* Begin: nav */}
+          <nav style={{display: "inline-block"}}><ul id="nav">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/projs">Projects</Link></li>
+            <li><Link to="/blogs">Blogs</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+            <li>
+              <span>
+                More
+                <AiOutlineDown className="nav-more-icon"/>
+              </span>
+              <ul className="subnav">
+                <li><Link to="/cv">CV</Link></li>
+              </ul>
+            </li>
+          </ul></nav>
+          {/* End: nav */}
+
+          {/* Begin: search button */}
+          <div className="search-btn">
+            <input name="search-param" placeholder="Search blogs..." style={ {width: "280px", height:"26px", marginTop:"9px", marginRight:"4px"} } />
+            <AiOutlineSearch style={{fontSize: "22px", color:"white", height: "42px", paddingTop: "2px", cursor: "pointer"}}/>
+          </div>
+          {/* End: search button */}
+        </div>
+
+        <div id="content">
+          <Routes>
+            <Route path="/" element={<HomePage/>} />
+            <Route path="/projs" element={<ProjectsList/>} />
+            <Route path="/proj"  element={<Project/>} />
+            <Route path="/blogs" element={<BlogsList/>} />
+            <Route path="/blog" element={<Blog/>} />
+            <Route path="/contact" element={<Contact/>} />
+            <Route path="/cv" element={<CV/>} />
+          </Routes>
+        </div>
+
+        <div id="footer">
+          <div>
+            Powered by ReactJS
+          </div>
+          <div>
+              &copy; huyj2ee.blogspot.com
+          </div>
+        </div>
+      </div>
+    );
+  }
+);
+
+export default App;
--- backend/frontend/src/index.js
+++ backend/frontend/src/index.js
@@ -1,137 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider, connect } from 'react-redux';
+import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import axios from 'axios';
 import axiosMiddleware from 'redux-axios-middleware';
-import { AiOutlineDown, AiOutlineSearch } from "react-icons/ai";
 import {
   BrowserRouter as Router,
-  Route,
-  Routes,
-  Link,
-  useLocation
 } from 'react-router-dom';
-import { BLOG_ID, ROOT_ID, API_KEY } from './utils/constant';
-import HomePage from './components/HomePage';
-import Contact from './components/Contact';
-import CV from './components/CV';
-import ProjectsList from './components/ProjectsList';
-import Project from './components/Project';
-import BlogsList from './components/BlogsList';
-import Blog from './components/Blog';
+import App from './components/App';
 import './style.css';
-
-const dispatchToPropsAppMap =
-  dispatch => {
-    return {
-      loadRoot: () => {
-        dispatch(
-          {
-            type: 'LOAD_ROOT',
-            payload: {
-              request: {
-                method: 'GET',
-                url: '/' + BLOG_ID + '/posts/' + ROOT_ID + '?key=' + API_KEY,
-                data: {
-                }
-              }
-            } 
-          }
-        )
-      },
-      clearProjectsList: () => {
-        dispatch(
-          {
-            type: 'CLEAR_PROJECTS_LIST'
-          }
-        )
-      },
-      clearBlogsList: () => {
-        dispatch(
-          {
-            type: 'CLEAR_BLOGS_LIST'
-          }
-        )
-      }
-    }
-  };
-
-const stateToPropsAppMap = (state) => {
-  return {
-      projectsList: state.projectsList,
-      blogsList: state.blogsList
-  };
-};
-
-const App = connect(stateToPropsAppMap, dispatchToPropsAppMap)(
-  function (props) {
-    const location = useLocation();
-
-    if(location.pathname !== '/projs' && location.pathname !== '/proj' && props.projectsList && typeof props.clearProjectsList === 'function')
-      props.clearProjectsList();
-
-    if(location.pathname !== '/blogs' && location.pathname !== '/blog' && props.blogsList && typeof props.clearBlogsList === 'function')
-      props.clearBlogsList();
-
-    useEffect(()=>{
-      if (typeof props.loadRoot === 'function')
-        props.loadRoot();
-    },[props]);
-
-    return (
-      <div id="main">
-        <div id="header">
-          {/* Begin: nav */}
-          <nav style={{display: "inline-block"}}><ul id="nav">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/projs">Projects</Link></li>
-            <li><Link to="/blogs">Blogs</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-            <li>
-              <span>
-                More
-                <AiOutlineDown className="nav-more-icon"/>
-              </span>
-              <ul className="subnav">
-                <li><Link to="/cv">CV</Link></li>
-              </ul>
-            </li>
-          </ul></nav>
-          {/* End: nav */}
-
-          {/* Begin: search button */}
-          <div className="search-btn">
-            <input name="search-param" placeholder="Search blogs..." style={ {width: "280px", height:"26px", marginTop:"9px", marginRight:"4px"} } />
-            <AiOutlineSearch style={{fontSize: "22px", color:"white", height: "42px", paddingTop: "2px", cursor: "pointer"}}/>
-          </div>
-          {/* End: search button */}
-        </div>
-
-        <div id="content">
-          <Routes>
-            <Route path="/" element={<HomePage/>} />
-            <Route path="/projs" element={<ProjectsList/>} />
-            <Route path="/proj"  element={<Project/>} />
-            <Route path="/blogs" element={<BlogsList/>} />
-            <Route path="/blog" element={<Blog/>} />
-            <Route path="/contact" element={<Contact/>} />
-            <Route path="/cv" element={<CV/>} />
-          </Routes>
-        </div>
-
-        <div id="footer">
-          <div>
-            Powered by ReactJS
-          </div>
-          <div>
-              &copy; huyj2ee.blogspot.com
-          </div>
-        </div>
-      </div>
-    );
-  }
-);
 
 const reducer = (state = 0, action) => {
   let obj = null;
