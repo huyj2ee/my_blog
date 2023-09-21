cf35a57e03fc66646c42e79dd3b781daa61400ab
huyj2ee <huyj2ee@outlook.com>
2023-09-21 13:20:52 +0700

    [uml] remove uml model

--- clean_uml_generated_files.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-rd /s /q uml\.runtime
-del uml\data\fragments\uml\admin\stamp.dat 2> NUL
--- clean_uml_generated_files.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm -f -r uml/.runtime
-rm -f uml/data/fragments/uml/admin/stamp.dat
